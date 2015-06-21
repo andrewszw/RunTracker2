939f201d8f9b9ad7cc49eb1da229faae2ed3e30d
andrewszw <zacharyandrews93@gmail.com>
2015-06-21 13:25:26 -0400

    made changes to rundatabasehelper and runmanager

--- app/src/main/java/com/bignerdranch/android/runtracker/RunManager.java
+++ app/src/main/java/com/bignerdranch/android/runtracker/RunManager.java
@@ -164,4 +164,8 @@ public class RunManager {
         cursor.close();
         return location;
     }
+
+    public LocationCursor queryLocationsForRun(long runId) {
+        return mHelper.queryLocationsForRun(runId);
+    }
 }
